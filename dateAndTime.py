877226098647c1e53aaf1741f298a1375322f24a
Vitthal <vitthalpadwal89@gmail.com>
2017-03-14 18:40:14 +0530

    Update dateAndTime.py

--- dateAndTime.py
+++ dateAndTime.py
@@ -1,6 +1,10 @@
 import datetime
-
+import getpass
 now = datetime.datetime.now()
 print("Current Date and time is")
 print str(now)
 print("Hello World")
+
+username = getpass.getuser()
+print(username)
+
